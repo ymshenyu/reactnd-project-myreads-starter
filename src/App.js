27e902efdb7b41c49c5e9a09b84f8a3da47e428f
ymshenyu <373318510@qq.com>
2020-04-08 08:55:59 +0800

    use Routes to control page content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route path='/search'>
           <div className="search-books">
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
@@ -37,7 +28,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        </Route>
+        <Route exact path='/'>
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -197,7 +189,7 @@ class BooksApp extends React.Component {
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
             </div>
           </div>
-        )}
+        </Route>
       </div>
     )
   }
